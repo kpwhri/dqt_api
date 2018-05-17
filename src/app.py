250114f6dbe271981f5a3381292bd1b6ed97622d
Cronkite <cronkite.d@ghc.org>
2018-05-17 10:45:05 -0700

    Merged changes in app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,9 @@
+import logging
+logging.basicConfig(  # troubleshoot initialization issues
+    filename=r'C:\wksp\log.txt',
+    filemode='a',
+    level=logging.DEBUG
+)
 from dqt_api import app, cors, whooshee
 import dqt_api.models
 import dqt_api.views
@@ -12,29 +18,33 @@ def mkdir_p(path):
     return path
 
 
+if not os.environ.get('FLASK_CONFIG_FILE', None):
+    raise ValueError('Environment variable FLASK_CONFIG_FILE not set.')
 app.config.from_pyfile(os.environ['FLASK_CONFIG_FILE'])
+try:
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['LOG_DIR'] = mkdir_p(os.path.join(app.config['BASE_DIR'], 'logs'))
+    app.config['SQLALCHEMY_MIGRATE_REPO'] = mkdir_p(os.path.join(app.config['BASE_DIR'], 'migrations'))
+    # app.config['WHOOSH_BASE'] = mkdir_p(os.path.join(app.config['BASE_DIR'], 'whooshee.idx'))
+    # app.config['WHOOSHEE_DIR'] = mkdir_p(os.path.join(app.config['BASE_DIR'], 'whooshee.idx'))
+    app.config['ALEMBIC'] = {
+        'script_location': mkdir_p(os.path.join(app.config['BASE_DIR'], 'migrations')),
+        'sqlalchemy.url': app.config['SQLALCHEMY_DATABASE_URI'],
+    }
+    app.config['WHOOSHEE_DIR'] = r'C:\wksp\dqt_api\src\whooshee'
+    app.debug = True
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['LOG_DIR'] = mkdir_p(os.path.join(app.config['BASE_DIR'], 'logs'))
-app.config['SQLALCHEMY_MIGRATE_REPO'] = mkdir_p(os.path.join(app.config['BASE_DIR'], 'migrations'))
-# app.config['WHOOSH_BASE'] = mkdir_p(os.path.join(app.config['BASE_DIR'], 'whooshee.idx'))
-# app.config['WHOOSHEE_DIR'] = mkdir_p(os.path.join(app.config['BASE_DIR'], 'whooshee.idx'))
-app.config['ALEMBIC'] = {
-    'script_location': mkdir_p(os.path.join(app.config['BASE_DIR'], 'migrations')),
-    'sqlalchemy.url': app.config['SQLALCHEMY_DATABASE_URI'],
-}
-app.config['WHOOSHEE_DIR'] = r'C:\wksp\dqt_api\src\whooshee'
-app.debug = True
+    app.secret_key = app.config['SECRET_KEY']
 
-app.secret_key = app.config['SECRET_KEY']
-
-handler = mylogging.EncryptedTimedRotatingFileHandler(
-    os.path.join(app.config['LOG_DIR'], 'log_file'),
-    app.config.get('LOG_KEY', None),
-    "midnight",
-    1
-)
-app.logger.addHandler(handler)
+    handler = mylogging.EncryptedTimedRotatingFileHandler(
+        os.path.join(app.config['LOG_DIR'], 'log_file'),
+        app.config.get('LOG_KEY', None),
+        "midnight",
+        1
+    )
+    app.logger.addHandler(handler)
+except Exception as e:
+    logging.error(e)
 
 cors.init_app(app, resources={r'/api/*': {'origins': app.config['ORIGINS']}})
 
@@ -44,17 +54,19 @@ try:
 except Exception as e:
     print('Failed to initialize whooshee.')
     print(e)
+    logger.error(e)
     app.logger.warning('Failed to initialize whooshee.')
     app.logger.exception(e)
 
 app_logged = TransLogger(app, logger=app.logger, setup_console_handler=False)
 cherrypy.tree.graft(app_logged, '/')
 cherrypy.tree.mount(None, '/static', config={})
+logging.debug(os.environ)
 cherrypy.config.update(
     {
         'engine.autoreload.on': False,
         'log.screen': True,
-        'server.socket_port': int(os.environ['PORT']),
+        'server.socket_port': int(os.environ['XPORT']),
         'server.socket_host': '0.0.0.0',
     }
 )
