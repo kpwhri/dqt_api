ff4390cd4caf3d65f4d2e42c13ed5c989b7a08ea
dcronkite <dcronkite@gmail.com>
2025-01-31 11:34:52 -0800

    Add app context when directly running load data dictioanry.

--- src/load_dd.py
+++ src/load_dd.py
@@ -19,12 +19,13 @@ if __name__ == '__main__':
     prepare_config()
 
     logger.add('load_data_dictionary_{time}.log', backtrace=True, diagnose=True)
-    add_data_dictionary(
-        args.dd_input_file,
-        args.dd_file_name,
-        args.dd_label_column,
-        args.dd_name_column,
-        args.dd_category_column,
-        args.dd_description_column,
-        args.dd_value_column,
-    )
+    with app.app_context():
+        add_data_dictionary(
+            args.dd_input_file,
+            args.dd_file_name,
+            args.dd_label_column,
+            args.dd_name_column,
+            args.dd_category_column,
+            args.dd_description_column,
+            args.dd_value_column,
+        )
